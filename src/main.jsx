129faee66f457e794627e419782c8e1c3163f480
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-13 16:41:39 +0700

    feat: add router import in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/globals.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import Router from "./Router";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Router />
     </BrowserRouter>
   </StrictMode>,
 );
