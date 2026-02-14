5be4b7d25b7a9081c7b6fba5c911b8ee7c3fb042
Virgil I Ambar <imanuelambar11@gmail.com>
2026-02-14 21:36:05 +0700

    feat: add new route for home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import store, { persistedStore } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import RegisterProvider from "./contexts/register/RegisterProvider";
+import HomeLayout from "./layout/HomeLayout";
+import Home from "./pages/Home";
 
 const router = createBrowserRouter([
   {
@@ -15,19 +17,28 @@ const router = createBrowserRouter([
     path: "/register",
     element: <Register />,
   },
+  {
+    path: "/",
+    element: <HomeLayout />,
+    children: [
+      {
+        index: true, // akan ditampilkan outlet
+        element: <Home />,
+      },
+    ],
+  },
 ]);
 
 const App = () => {
   return (
-    <>
-      <Provider store={store}>
-        <PersistGate persistor={persistedStore}>
-          <RegisterProvider>
-            <RouterProvider router={router} />
-          </RegisterProvider>
-        </PersistGate>
-      </Provider>
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <RegisterProvider>
+          <RouterProvider router={router} />
+        </RegisterProvider>
+      </PersistGate>
+    </Provider>
   );
 };
+
 export default App;
